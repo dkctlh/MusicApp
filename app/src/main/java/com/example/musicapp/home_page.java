a0a632b0053e8b72270eed8de66f26e7b6ef29d5
dkctlh <dikicitalha@gmail.com>
2022-05-02 01:25:28 +0300

    rounded image update

--- app/src/main/java/com/example/musicapp/home_page.java
+++ app/src/main/java/com/example/musicapp/home_page.java
@@ -1,14 +1,61 @@
 package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class home_page extends AppCompatActivity {
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        ActivityCompat.requestPermissions(home_page.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.MANAGE_EXTERNAL_STORAGE},1);
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted and now can proceed
+                    recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+                    MusicAdapter musicAdapter = new MusicAdapter(this, Music.getData());
+                    recyclerView.setAdapter(musicAdapter);
+
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                    recyclerView.setLayoutManager(linearLayoutManager);
+
+                    Toast.makeText(home_page.this, "Permission granted", Toast.LENGTH_SHORT).show();
+
+                  //  adapter= new ArrayAdapter<String>(getApplicationContext(),android)
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(home_page.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // add other cases for more permissions
+        }
+    }
+    public void Allsongs(View v){
+        Intent alls_i=new Intent(this,AllSongs.class);
+        startActivity(alls_i);
     }
 }
